7def34873a3f18fe2b24a0a5c28359931f3f6dc6
wlodos <vova30111194@gmail.com>
2021-12-27 22:50:21 +0100

    new file added

---
+++ Python.JSON and Class object/testCat.py
@@ -0,0 +1,17 @@
+import json
+from cat import Cat
+
+def check_cat(elem, item):
+    if elem[item]['code'] == 'cat':
+        print(elem)
+        print(elem['name'], elem['gender']['name'], elem['age'])
+        return True
+
+with open('f.json', encoding='utf8') as file:
+    templates = json.load(file)
+
+for elem in templates['results']:
+    for item in elem:
+        if item == 'species' and check_cat(elem, item):
+            obj_Cat = Cat(elem['name'], elem['gender']['name'], elem['age'])
+            print(obj_Cat.name)
